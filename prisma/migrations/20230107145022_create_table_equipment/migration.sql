ff45298b987aed17f87c4af4d3f90e63ce59dc5d
Patrick Matos <pmatos96@hotmail.com>
2023-01-07 12:12:00 -0300

    Intial setup

---
+++ prisma/migrations/20230107145022_create_table_equipment/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Equipment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "power" TEXT NOT NULL,
+    "isResistive" BOOLEAN NOT NULL
+);
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+async function bootstrap(){
+
+    const fastify = Fastify({
+        logger: true
+    })
+
+    fastify.get('/equipments', async () => {
+
+
+        const equipments = await prisma.equipment.findMany({
+
+        });
+
+        return { equipments }
+    })
+
+    await fastify.listen({ port: 3333 })
+}
+
+bootstrap();
