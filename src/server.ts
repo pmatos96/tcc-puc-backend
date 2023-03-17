14d67ec21ae45a4a988409c12acf04fd7f8cbcc4
Patrick Matos <pmatos96@hotmail.com>
2023-03-16 22:25:48 -0300

    Project creation route

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,24 @@ async function bootstrap(){
         origin: true // TODO - Adaptar o domínio do front
     })
 
+    fastify.post('/projects/', async (request: FastifyRequest<{
+        Body: {
+            name: string
+        }
+    }>, response) => {
+        
+        const { name } = request.body;
+
+        const newProject = await prisma.project.create({
+            data: {
+                name,
+                ownerId: "clcxsutm50000j938pytsfypo"
+            }
+        })
+
+        response.status(201).send(newProject);
+    })
+
     fastify.get('/equipments/', async (request: FastifyRequest<{
         Querystring: {
             typeId: string
@@ -74,6 +92,13 @@ async function bootstrap(){
         })
 
         return { projectItems }
+    })
+
+    fastify.get('/rooms/', async (request: FastifyRequest) => {
+
+        let rooms = await prisma.room.findMany();
+
+        return { rooms }
     })
 
     await fastify.listen({ port: 3333 })
